4fabb08e3cac7c10ec07cd363c4cc317f104a1f5
Johan Haugland <johanhaugl@gmail.com>
2022-09-06 10:48:33 +0200

    implement cards with delete card feature

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Card from "./Card.svelte";
+  export let item = {};
+
+  const dispatch = createEventDispatcher();
+
+  let deleteFeedback = (itemID) => {
+    dispatch("delete-feedback", itemID);
+  };
+</script>
+
+<Card>
+  <div class="num-display">{item.rating}</div>
+  <div class="close" on:click={() => deleteFeedback(item.id)}>X</div>
+  <p class="text-display">{item.text}</p>
+</Card>
+
+<style>
+  .num-display {
+    position: absolute;
+    top: -10px;
+    left: -10px;
+    color: #fff;
+    background-color: #ff6a95;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    border: 1px #eee solid;
+    text-align: center;
+    padding: 10px;
+    font-size: 19px;
+  }
+
+  .close {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,10 @@
+<script>
+  import FeedbackItem from "./FeedbackItem.svelte";
+  export let feedback = [];
+</script>
+
+<div>
+  {#each feedback as fb}
+    <FeedbackItem item={fb} on:delete-feedback />
+  {/each}
+</div>
