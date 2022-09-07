fff831facc9e0826829b85c995481ec42fdc197a
Johan Haugland <johanhaugl@gmail.com>
2022-09-07 15:54:45 +0200

    Versjon med nye reviews

--- src/stores.js
+++ src/stores.js
@@ -4,16 +4,21 @@ export const FeedbackStore = writable([
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Hei på deg, Silje!',
   },
   {
     id: 2,
     rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Nå drar jeg til Stockholm',
   },
   {
     id: 3,
     rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Håper du får en 10/10 slutt på arbeidsuka',
+  },
+  {
+    id: 4,
+    rating: 10,
+    text: 'Du er kul og jeg er glad i deg <3',
   },
 ]);
